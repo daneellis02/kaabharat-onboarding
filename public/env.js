ce398e98b719c0b258368b603f4bd0e4ab60a4cc
Kstv <Koustav@sunflowerinfomedia.in>
2025-08-11 13:50:04 +0530

    Add files via upload

---
+++ public/env.js
@@ -0,0 +1,8 @@
+// IMPORTANT: Replace "YOUR_GEMINI_API_KEY_HERE" with your actual Google Gemini API key.
+// This file should be kept private and not be committed to public version control.
+
+window.process = {
+  env: {
+    API_KEY: "AIzaSyCtH7xmO22P4a6Uj7vuUjMOPVVurF-W34Y"
+  }
+};
